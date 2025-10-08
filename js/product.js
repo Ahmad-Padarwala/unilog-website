63a1ce2f782a0ecd0de4a9c284d59564c26f4a04
Ahmad-Padarwala <ahmadpadarwala4@gmail.com>
2025-10-08 15:03:26 +0530

    redesign download broucher section

--- js/product.js
+++ js/product.js
@@ -297,5 +297,18 @@ document.addEventListener("DOMContentLoaded", function () {
     (function () {
         emailjs.init("IY_g9yid_NrS6B3EZ");
     })();
-    
 });
+
+// after before slider
+var divisor = document.getElementById("divisor"),
+    handle = document.getElementById("handle"),
+    slider = document.getElementById("slider");
+
+function moveDivisor() {
+    handle.style.left = slider.value + "%";
+    divisor.style.width = slider.value + "%";
+}
+
+window.onload = function () {
+    moveDivisor();
+};
